ca838d1820501ed49eba56aad0d527885fc45c3c
dongjun6343 <dongjun6343@naver.com>
2022-08-16 22:21:46 +0900

    스프링 JPA Initial commit

---
+++ src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package study.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTests {
+	// gradle을 통해서 띄움. --> 느리기 때문에 설정변경하기.
+	// ctrl+alt+s
+	// 1. 빌드도구 --> gradle
+	// 2. 어노테이션 프로세서 --> 활성화 체크하기.
+	@Test
+	void contextLoads() {
+	}
+
+}
