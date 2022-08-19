983fcba28f2c7b75fef91efed22b7d0a01a030c4
dongjun6343 <dongjun6343@naver.com>
2022-08-19 20:57:03 +0900

    도메인 모델 commit

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,24 +1,43 @@
 package study.datajpa.entity;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// 객체를 찍을때 출력.
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-        
-    }
+    // 멤버랑 팀은 다대일 관계
+    @ManyToOne
+    @JoinColumn(name = "team_id") //FK명
+    private Team team;
+// JPA에서는 protected로 기본생성자를 만들어줘야함. --> @NoArgsConstructor(access = AccessLevel.PROTECTED)로 밑에꺼 대신.
+//    protected Member() {
+//
+//    }
 
     public Member(String username) {
         this.username = username;
+    }
+
+    //멤버가 팀을 변경한다.
+    public void changeTeam(Team team){
+        this.team = team;
+        // 연관관계에 있는 team에 있는 member에도 세팅.
+        // 객체이므로 관계가 있는 쪽에도 세팅해줘야함.
+        team.getMembers().add(this);
     }
 }
