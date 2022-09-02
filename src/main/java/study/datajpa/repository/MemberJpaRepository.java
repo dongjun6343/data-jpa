4e7040c5611c77673ca5162c3e93913c50fca63e
dongjun6343 <dongjun6343@naver.com>
2022-09-02 11:00:30 +0900

    순수 JPA 페이징과 정렬

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -53,4 +53,19 @@ public class MemberJpaRepository {
                 .setParameter("age", age)
                 .getResultList();
     }
+
+    // 순수 JPA 페이징과 정렬
+    public List<Member> findByPage(int age, int offset, int limit){
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+    public long totalCount(int age){
+        // count는 한건이므로 getSingleResult
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
