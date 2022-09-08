b19016b2df8461cb0cedc7d9ecb9ca7f3f6ae2f3
dongjun6343 <dongjun6343@naver.com>
2022-09-08 11:11:07 +0900

    사용자 정의 리포지토리 구현

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 // Java의 Proxy 기술로 가짜 클래스를 만든 후 주입을 해줘서 구현체가 없어도 사용할 수 있다.
 // memberRepository.getClass() --> class com.sun.proxy.$ProxyXXX
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
     // 메소드이름으로 쿼리생성 -> 쿼리 메소드 기능
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
